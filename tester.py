36a7641c3ecfb3512981acf4385e37dfadf55cf5
CorLaHu <ordinemlars@gmail.com>
2022-10-26 12:18:02 +0200

    Added Matplotlib graph

--- tester.py
+++ tester.py
@@ -1,26 +1,49 @@
 from time import perf_counter
 from os import system
 import subprocess
+import matplotlib.pyplot as plt
+import numpy as np
 
 nbodypy = r"C:\Users\ordin\Documents\Github\GEO1000_Assignment_4\nbody.py"
 nbodyexe = r"C:\Users\ordin\Documents\Github\GEO1000_Assignment_4\cmake-build-release\nbody.exe"
 iterations = [5000, 500000, 5000000]
-
-times = {"c": [], "py": []}
+ctimes = []
+pytimes = []
 
 for iteration in iterations:
 	print(f"Currently running iteration number {iteration} in C++")
 	t1 = perf_counter()
 	system(f"{nbodyexe} {iteration}")
 	t2 = perf_counter()
-	times["c"].append(t2 - t1)
+	ctimes.append(t2 - t1)
 
 for iteration in iterations:
 	print(f"Currently running iteration number {iteration} in Python")
 	t1 = perf_counter()
 	system(f"python {nbodypy} {iteration}")
 	t2 = perf_counter()
-	times["py"].append(t2 - t1)
+	pytimes.append(t2 - t1)
 
-print(times["c"])
-print(times["py"])
+print(ctimes)
+print(pytimes)
+
+
+labels = ["5000", "500000", "5000000"]
+x = np.arange(len(labels))
+width = 0.35
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, ctimes, width, label='C++')
+rects2 = ax.bar(x + width/2, pytimes, width, label='Python')
+plt.yscale('log')
+
+ax.set_ylabel('Runtime in seconds')
+ax.set_xlabel('Instance size')
+ax.set_xticks(x, labels)
+ax.legend()
+
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+
+fig.tight_layout()
+
+plt.savefig("exportgraph.png")
