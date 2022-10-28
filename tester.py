6286b161bb5ff81d222648026af2b7d6aa67457c
CorLaHu <ordinemlars@gmail.com>
2022-10-28 10:49:26 +0200

    Fixed the python write flag, added result rounding

--- tester.py
+++ tester.py
@@ -4,9 +4,9 @@ import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 
-nbodypy = r"C:\Users\ordin\Documents\Github\GEO1000_Assignment_4\nbody.py"
-nbodyrelexe = r"C:\Users\ordin\Documents\Github\GEO1000_Assignment_4\cmake-build-release\nbody.exe"
-nbodydebexe = r"C:\Users\ordin\Documents\Github\GEO1000_Assignment_4\cmake-build-debug\nbody.exe"
+nbodypy = r"C:\Users\Durspy\Documents\GitHub\GEO1000_Assignment_4\nbody.py"
+nbodyrelexe = r"C:\Users\Durspy\Documents\GitHub\GEO1000_Assignment_4\cmake-build-release\nbody.exe"
+nbodydebexe = r"C:\Users\Durspy\Documents\GitHub\GEO1000_Assignment_4\cmake-build-debug\nbody.exe"
 iterations = [5000, 500000, 5000000, 50000000]
 
 creltimes = []
@@ -27,12 +27,22 @@ for iteration in iterations:
 	t2 = perf_counter()
 	creltimes.append(t2 - t1)
 
+
 for iteration in iterations:
 	print(f"Currently running iteration size number {iteration} in Python")
 	t1 = perf_counter()
 	system(f"python {nbodypy} {iteration}")
 	t2 = perf_counter()
 	pytimes.append(t2 - t1)
+
+print(cdebtimes)
+print(creltimes)
+print(pytimes)
+
+for (a, b, c, i) in zip(cdebtimes, creltimes, pytimes, range(len(iterations))):
+	cdebtimes[i] = round(a, 3)
+	creltimes[i] = round(b, 3)
+	pytimes[i] = round(c, 3)
 
 print(cdebtimes)
 print(creltimes)
